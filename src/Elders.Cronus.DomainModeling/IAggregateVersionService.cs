81bb18ad93b9a9b7cea28d2e35d521180b68b152
Blagovest Petrov <blagovest.petrov@mentormate.com>
2014-11-14 13:16:17 +0200

    Added IAggregateVersionService interface.

---
+++ src/Elders.Cronus.DomainModeling/IAggregateVersionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elders.Cronus.DomainModeling
+{
+    public interface IAggregateVersionService : IDisposable
+    {
+        int ReserveVersion(IAggregateRootId aggregateId, int requestedVersion);
+    }
+}

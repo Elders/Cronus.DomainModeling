dd4095a1cb59d5f48d27a93702e11646a424fb13
mynkow <mynkow@gmail.com>
2015-06-18 12:53:49 +0300

    Add method TryLoad when loading aggregates from the event store

--- src/Elders.Cronus.DomainModeling/IAggregateRepository.cs
+++ src/Elders.Cronus.DomainModeling/IAggregateRepository.cs
@@ -6,12 +6,15 @@ namespace Elders.Cronus.DomainModeling
     /// Indicates the ability to store and retreive a stream of events.
     /// </summary>
     /// <remarks>
-    /// Instances of this class must be designed to be thread safe such that they can be shared between threads.
+    /// Implementations of this interface must be designed to be thread safe such that they can be shared between threads and
+    /// machines.
     /// </remarks>
     public interface IAggregateRepository
     {
         void Save<AR>(AR aggregateRoot) where AR : IAggregateRoot;
 
         AR Load<AR>(IAggregateRootId id) where AR : IAggregateRoot;
+
+        bool TryLoad<AR>(IAggregateRootId id, out AR aggregateRoot) where AR : IAggregateRoot;
     }
 }

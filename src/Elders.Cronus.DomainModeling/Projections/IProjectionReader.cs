117d6bf116739f3600363658b1c2afe93959841c
mynkow <mynkow@gmail.com>
2018-10-16 13:54:42 +0300

    Renames IProjectionLoader to IProjectionReader

--- src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
+++ src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Elders.Cronus.Projections
 {
-    public interface IProjectionLoader
+    public interface IProjectionReader
     {
         IProjectionGetResult<T> Get<T>(IBlobId projectionId) where T : IProjectionDefinition;
         IProjectionGetResult<IProjectionDefinition> Get(IBlobId projectionId, Type projectionType);

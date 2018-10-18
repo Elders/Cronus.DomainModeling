3bbd52230c2e683264663dec8946ea684f8b8b65
mynkow <mynkow@gmail.com>
2018-10-18 09:17:05 +0300

    Replaces `IProjectionGetResult` with `ReadResult`

--- src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
+++ src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
@@ -5,10 +5,10 @@ namespace Elders.Cronus.Projections
 {
     public interface IProjectionReader
     {
-        IProjectionGetResult<T> Get<T>(IBlobId projectionId) where T : IProjectionDefinition;
-        IProjectionGetResult<IProjectionDefinition> Get(IBlobId projectionId, Type projectionType);
+        ReadResult<T> Get<T>(IBlobId projectionId) where T : IProjectionDefinition;
+        ReadResult<IProjectionDefinition> Get(IBlobId projectionId, Type projectionType);
 
-        Task<IProjectionGetResult<T>> GetAsync<T>(IBlobId projectionId) where T : IProjectionDefinition;
-        Task<IProjectionGetResult<IProjectionDefinition>> GetAsync(IBlobId projectionId, Type projectionType);
+        Task<ReadResult<T>> GetAsync<T>(IBlobId projectionId) where T : IProjectionDefinition;
+        Task<ReadResult<IProjectionDefinition>> GetAsync(IBlobId projectionId, Type projectionType);
     }
 }
---
+++ src/Elders.Cronus.DomainModeling/ReadResult.cs
@@ -0,0 +1,25 @@
+﻿namespace Elders.Cronus
+{
+    public struct ReadResult<T>
+    {
+        public ReadResult(string error) : this(default(T), error) { }
+
+        public ReadResult(T data, string error = "")
+        {
+            Data = data;
+            Error = error;
+        }
+
+        public ReadResult(System.Exception exception) : this(exception.Message) { }
+
+        public T Data { get; set; }
+
+        public string Error { get; set; }
+
+        public bool NotFound => (Data.Equals(default(T)));
+
+        public bool IsSuccess => HasFailed == false;
+
+        public bool HasFailed => NotFound || string.IsNullOrEmpty(Error) == false;
+    }
+}

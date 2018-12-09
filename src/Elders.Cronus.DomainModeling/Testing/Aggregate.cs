71cf92e286b89b0ad9e4dc0eaa4552ef1f923fd7
mynkow <mynkow@gmail.com>
2018-12-09 14:31:33 +0200

    Adds helper classes for easier testing

---
+++ src/Elders.Cronus.DomainModeling/Testing/Aggregate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Elders.Cronus.Testing
+{
+    public static class Aggregate<T> where T : IAggregateRoot
+    {
+        public static AggregateRootHistory FromHistory()
+        {
+            return new AggregateRootHistory();
+        }
+
+        public class AggregateRootHistory
+        {
+            public AggregateRootHistory()
+            {
+                Events = new List<IEvent>();
+            }
+
+            public List<IEvent> Events { get; set; }
+
+            public AggregateRootHistory Event(IEvent @event)
+            {
+                Events.Add(@event);
+                return this;
+            }
+
+            public T Build()
+            {
+                var instance = (T)Activator.CreateInstance(typeof(T), true);
+                instance.ReplayEvents(Events, 1);
+                return instance;
+            }
+        }
+    }
+}
---
+++ src/Elders.Cronus.DomainModeling/Testing/AggregateRootExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Elders.Cronus.Testing
+{
+    public static class AggregateRootExtensions
+    {
+        public static T PublishedEvent<T>(this IAggregateRoot root) where T : IEvent
+        {
+            var @event = root.UncommittedEvents.SingleOrDefault(x => x is T);
+            if (ReferenceEquals(@event, null) || @event.Equals(default(T)))
+                return default(T);
+            return (T)@event;
+        }
+
+        public static bool IsEventPublished<T>(this IAggregateRoot root) where T : IEvent
+        {
+            return ReferenceEquals(default(T), PublishedEvent<T>(root)) == false;
+        }
+
+        public static bool HasNewEvents(this IAggregateRoot root)
+        {
+            return root.UncommittedEvents.Any();
+        }
+
+        public static T RootState<T>(this AggregateRoot<T> root) where T : IAggregateRootState, new()
+        {
+            return (T)(root as IHaveState<IAggregateRootState>).State;
+        }
+    }
+}

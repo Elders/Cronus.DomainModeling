441100793d9eab7a47a10f7abc7b578a866e25b5
mynkow <mynkow@gmail.com>
2020-04-10 23:27:42 +0300

    Adds a markup interface for public events which are part of a domain's published language

---
+++ src/Elders.Cronus.DomainModeling/IPublicEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Elders.Cronus
+{
+    /// <summary>
+    /// An event which is part of the domain's Published Language
+    /// </summary>
+    public interface IPublicEvent : IMessage { }
+}

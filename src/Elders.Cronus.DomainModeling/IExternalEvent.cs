9ba6d4712be4645df9633e0102c483b3557afec9
mynkow <mynkow@gmail.com>
2020-04-10 23:27:42 +0300

    Adds a markup interface for public events which are part of a domain's published language

--- src/Elders.Cronus.DomainModeling/IExternalEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Elders.Cronus
-{
-    /// <summary>
-    /// External domain events represent business changes which already happened in another bounded context
-    /// </summary>
-    public interface IExternalEvent : IMessage { }
-}

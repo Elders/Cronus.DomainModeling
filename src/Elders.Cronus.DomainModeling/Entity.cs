dc2ddb004572ac9b6c7b85a64ebbb8af357f84ef
mynkow <mynkow@gmail.com>
2020-05-26 19:45:52 +0300

    Adds an extension for extracting an Entity state.

--- src/Elders.Cronus.DomainModeling/Entity.cs
+++ src/Elders.Cronus.DomainModeling/Entity.cs
@@ -30,4 +30,17 @@
             ar.Apply((dynamic)entityEvent);
         }
     }
+
+    public static class EntityExtensions
+    {
+        public static T GetState<T>(this IHaveState<T> entity) where T : IEntityState => entity.State;
+
+        public static TEntityState State<TAggregateRoot, TEntityState>(this Entity<TAggregateRoot, TEntityState> entity)
+            where TAggregateRoot : IAggregateRoot
+            where TEntityState : IEntityState, new()
+        {
+            return (TEntityState)GetState(entity);
+        }
+
+    }
 }

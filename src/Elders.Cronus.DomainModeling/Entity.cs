351b782b49ee00cdd991e2b0225074287be64725
mynkow <mynkow@gmail.com>
2020-05-30 20:12:12 +0300

    Moves the EntityExtensions to a file

--- src/Elders.Cronus.DomainModeling/Entity.cs
+++ src/Elders.Cronus.DomainModeling/Entity.cs
@@ -30,17 +30,4 @@
             ar.Apply((dynamic)entityEvent);
         }
     }
-
-    public static class EntityExtensions
-    {
-        public static T GetState<T>(this IHaveState<T> entity) where T : IEntityState => entity.State;
-
-        public static TEntityState State<TAggregateRoot, TEntityState>(this Entity<TAggregateRoot, TEntityState> entity)
-            where TAggregateRoot : IAggregateRoot
-            where TEntityState : IEntityState, new()
-        {
-            return (TEntityState)GetState(entity);
-        }
-
-    }
 }

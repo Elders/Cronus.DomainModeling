8feb90b805f2b593a904b74c9f8fa71c03b81cb6
mynkow <mynkow@gmail.com>
2021-11-05 18:41:50 +0200

    fix: Adds documentation

--- src/Elders.Cronus.DomainModeling/Projections/IAmEventSourcedProjection.cs
+++ src/Elders.Cronus.DomainModeling/Projections/IAmEventSourcedProjection.cs
@@ -2,6 +2,9 @@
 
 namespace Elders.Cronus.Projections
 {
+    /// <summary>
+    /// Specifies that a projection supports event sourcing.
+    /// </summary>
     public interface IAmEventSourcedProjection
     {
         void ReplayEvents(IEnumerable<IEvent> events);

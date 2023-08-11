9f3cfc03e3c7aca4e1f7de340147b0f75073c077
Georgi Shukov <georgi.shukov98@gmail.com>
2023-08-11 14:20:37 +0300

    fix: Extends one of the overloads of Publish

--- src/Elders.Cronus.DomainModeling/IPublisher.cs
+++ src/Elders.Cronus.DomainModeling/IPublisher.cs
@@ -31,11 +31,12 @@ public interface IPublisher<in TMessage> where TMessage : IMessage
     bool Publish(TMessage message, TimeSpan publishAfter, Dictionary<string, string> messageHeaders = null);
 
     /// <summary>
-    /// Publishes the speicified message bytes. You need to take care about the correct <paramref name="messageType"/>
+    /// Publishes the speicified message bytes. You need to take care about the correct <paramref name="messageType"/> and <paramref name="tenant"/>
     /// </summary>
     /// <param name="messageRaw">The message bytes.</param>
     /// <param name="messageType">The type of the message.</param>
+    /// <param name="tenant">The tenant of the message</param>
     /// <param name="messageHeaders">The message headers.</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    bool Publish(byte[] messageRaw, Type messageType, Dictionary<string, string> messageHeaders);
+    bool Publish(byte[] messageRaw, Type messageType, string tenant, Dictionary<string, string> messageHeaders);
 }

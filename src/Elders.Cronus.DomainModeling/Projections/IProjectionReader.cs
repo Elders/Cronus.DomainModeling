264025b383164f39c114c4a61e72497f65592de4
mynkow <mynkow@gmail.com>
2024-01-04 07:57:44 +0200

    feat: Get a projection and its state as of specific point in time.

--- src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
+++ src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
@@ -6,5 +6,7 @@ namespace Elders.Cronus.Projections;
 public interface IProjectionReader
 {
     Task<ReadResult<T>> GetAsync<T>(IBlobId projectionId) where T : IProjectionDefinition;
+    Task<ReadResult<T>> GetAsOfAsync<T>(IBlobId projectionId, DateTimeOffset timestamp) where T : IProjectionDefinition;
+
     Task<ReadResult<IProjectionDefinition>> GetAsync(IBlobId projectionId, Type projectionType);
 }

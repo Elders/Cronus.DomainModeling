1fd811fe353eb6973b113c0fd24c63eacc0ed58d
Kalina Shupeva <kalina.shupeva@eldersoss.com>
2024-03-12 10:26:45 +0200

    Adds additional GetAsOfAsync method that accepts projectionType parameter

--- src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
+++ src/Elders.Cronus.DomainModeling/Projections/IProjectionReader.cs
@@ -9,4 +9,5 @@ public interface IProjectionReader
     Task<ReadResult<T>> GetAsOfAsync<T>(IBlobId projectionId, DateTimeOffset timestamp) where T : IProjectionDefinition;
 
     Task<ReadResult<IProjectionDefinition>> GetAsync(IBlobId projectionId, Type projectionType);
+    Task<ReadResult<IProjectionDefinition>> GetAsOfAsync(IBlobId projectionId, Type projectionType, DateTimeOffset timestamp);
 }

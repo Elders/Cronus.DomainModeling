2df95915692e2add51346441f93c1946cea28fc8
mynkow <mynkow@gmail.com>
2025-02-11 15:49:33 +0200

    major: Makes publisher async

--- src/Elders.Cronus.DomainModeling/IPublisher.cs
+++ src/Elders.Cronus.DomainModeling/IPublisher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Elders.Cronus;
 
@@ -11,7 +12,7 @@ public interface IPublisher<in TMessage> where TMessage : IMessage
     /// <param name="message">The message</param>
     /// <param name="messageHeaders">The message headers</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    bool Publish(TMessage message, Dictionary<string, string> messageHeaders = null);
+    Task<bool> PublishAsync(TMessage message, Dictionary<string, string> messageHeaders = null);
 
     /// <summary>
     /// Publishes the specified message.
@@ -19,7 +20,7 @@ public interface IPublisher<in TMessage> where TMessage : IMessage
     /// <param name="message">The message.</param>
     /// <param name="publishAt">The specific point in time when the message will be published.</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    bool Publish(TMessage message, DateTime publishAt, Dictionary<string, string> messageHeaders = null);
+    Task<bool> PublishAsync(TMessage message, DateTime publishAt, Dictionary<string, string> messageHeaders = null);
 
     /// <summary>
     /// Publishes the specified message.
@@ -28,7 +29,7 @@ public interface IPublisher<in TMessage> where TMessage : IMessage
     /// <param name="publishAfter">The timespan after current time which the message will be published.</param>
     /// <param name="messageHeaders">The message headers.</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    bool Publish(TMessage message, TimeSpan publishAfter, Dictionary<string, string> messageHeaders = null);
+    Task<bool> PublishAsync(TMessage message, TimeSpan publishAfter, Dictionary<string, string> messageHeaders = null);
 
     /// <summary>
     /// Publishes the specified message bytes. You need to take care about the correct <paramref name="messageType"/> and <paramref name="tenant"/>
@@ -38,5 +39,5 @@ public interface IPublisher<in TMessage> where TMessage : IMessage
     /// <param name="tenant">The tenant of the message</param>
     /// <param name="messageHeaders">The message headers.</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    bool Publish(byte[] messageRaw, Type messageType, string tenant, Dictionary<string, string> messageHeaders);
+    Task<bool> PublishAsync(byte[] messageRaw, Type messageType, string tenant, Dictionary<string, string> messageHeaders);
 }

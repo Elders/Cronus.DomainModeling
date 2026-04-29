a1805199a074f77ff98e83d76f54c100be648553
Kalin Venkov <kalin.venkov@eldersoss.com>
2026-04-29 13:16:15 +0300

    feat: Adds CancellationToken to IPublisher overloads

--- src/Elders.Cronus.DomainModeling/IPublisher.cs
+++ src/Elders.Cronus.DomainModeling/IPublisher.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Elders.Cronus;
@@ -7,37 +8,43 @@ namespace Elders.Cronus;
 public interface IPublisher<in TMessage> where TMessage : IMessage
 {
     /// <summary>
-    /// Publishes the specified message.
+    /// Publishes the specified message asynchronously.
     /// </summary>
-    /// <param name="message">The message</param>
-    /// <param name="messageHeaders">The message headers</param>
+    /// <param name="message">The message to publish.</param>
+    /// <param name="messageHeaders">Optional headers to attach to the message.</param>
+    /// <param name="cancellationToken">A token to cancel the in-flight publish.</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    Task<bool> PublishAsync(TMessage message, Dictionary<string, string> messageHeaders = null);
+    Task<bool> PublishAsync(TMessage message, Dictionary<string, string> messageHeaders = null, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Publishes the specified message.
+    /// Schedules the specified message to be published at a specific point in time.
     /// </summary>
-    /// <param name="message">The message.</param>
-    /// <param name="publishAt">The specific point in time when the message will be published.</param>
+    /// <param name="message">The message to publish.</param>
+    /// <param name="publishAt">The UTC point in time when the message will be published.</param>
+    /// <param name="messageHeaders">Optional headers to attach to the message.</param>
+    /// <param name="cancellationToken">A token to cancel the in-flight publish (does not cancel the scheduled delivery itself).</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    Task<bool> PublishAsync(TMessage message, DateTime publishAt, Dictionary<string, string> messageHeaders = null);
+    Task<bool> PublishAsync(TMessage message, DateTime publishAt, Dictionary<string, string> messageHeaders = null, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Publishes the specified message.
+    /// Schedules the specified message to be published after the given delay.
     /// </summary>
-    /// <param name="message">The message</param>
-    /// <param name="publishAfter">The timespan after current time which the message will be published.</param>
-    /// <param name="messageHeaders">The message headers.</param>
+    /// <param name="message">The message to publish.</param>
+    /// <param name="publishAfter">The timespan after the current time at which the message will be published.</param>
+    /// <param name="messageHeaders">Optional headers to attach to the message.</param>
+    /// <param name="cancellationToken">A token to cancel the in-flight publish (does not cancel the scheduled delivery itself).</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    Task<bool> PublishAsync(TMessage message, TimeSpan publishAfter, Dictionary<string, string> messageHeaders = null);
+    Task<bool> PublishAsync(TMessage message, TimeSpan publishAfter, Dictionary<string, string> messageHeaders = null, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Publishes the specified message bytes. You need to take care about the correct <paramref name="messageType"/> and <paramref name="tenant"/>
+    /// Publishes a pre-serialized message asynchronously. The caller is responsible for supplying the correct
+    /// <paramref name="messageType"/> and <paramref name="tenant"/>; no validation is performed against <paramref name="messageRaw"/>.
     /// </summary>
-    /// <param name="messageRaw">The message bytes.</param>
-    /// <param name="messageType">The type of the message.</param>
-    /// <param name="tenant">The tenant of the message</param>
-    /// <param name="messageHeaders">The message headers.</param>
+    /// <param name="messageRaw">The serialized message bytes.</param>
+    /// <param name="messageType">The runtime <see cref="Type"/> of the message.</param>
+    /// <param name="tenant">The tenant the message belongs to.</param>
+    /// <param name="messageHeaders">Optional headers to attach to the message.</param>
+    /// <param name="cancellationToken">A token to cancel the in-flight publish.</param>
     /// <returns>Returns true if sending the message was successful.</returns>
-    Task<bool> PublishAsync(byte[] messageRaw, Type messageType, string tenant, Dictionary<string, string> messageHeaders);
+    Task<bool> PublishAsync(byte[] messageRaw, Type messageType, string tenant, Dictionary<string, string> messageHeaders = null, CancellationToken cancellationToken = default);
 }
